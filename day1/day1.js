6ae35eb9715a0fdd4aa738e216be1e6ead7925e6
Carlos Baumont <baumont.carlos@gmail.com>
2022-12-03 13:47:30 +0000

    Day 1 puzzle second half

--- day1/day1.js
+++ day1/day1.js
@@ -8,17 +8,30 @@ const rl = readline.createInterface({
 
 let totalCalories = 0;
 let maxCalories = 0;
+let caloriesElves = new Set();
 
 const sumCalories = ((calories) => {
     totalCalories = totalCalories + calories;
 })
 
 const checkMaxCalories = (() => {
+    caloriesElves.add(totalCalories);
     if (totalCalories >= maxCalories) {
         maxCalories = totalCalories;
     }
-    console.log(maxCalories);
+    console.log(`Top 1 elf is carrying in total ${maxCalories} calories.`);
     totalCalories = 0;
+    topThreeElves();
+})
+
+const topThreeElves = (() => {
+    const topElves = [...caloriesElves];
+    topElves.sort(function (a, b) { return b - a });
+    let sumTopCalories = 0;
+    for (const value of topElves.slice(0, 3)) {
+        sumTopCalories += value;
+    }
+    console.log(`Top 3 elves are carrying in total ${sumTopCalories} calories.`);
 })
 
 rl.on('line', (line) => {
