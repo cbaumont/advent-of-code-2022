88f5bf7f1da202be537798de9fa516e2381ba4fe
Carlos Baumont <baumont.carlos@gmail.com>
2022-12-04 10:58:19 +0000

    Finishing day 2 second part

--- day2/day2.js
+++ day2/day2.js
@@ -1,23 +1,24 @@
 import { rl as readline } from "../index.js";
 
 let totalScore = 0;
+let totalScorePartTwo = 0;
 
 function getShapeScore(shape) {
-    if (shape === 'A' || shape === 'X') {
+    if (shape === 'A' || shape === 'X') { //rock
         return 1;
     }
-    if (shape === 'B' || shape === 'Y') {
+    if (shape === 'B' || shape === 'Y') { //paper
         return 2;
     }
-    if (shape === 'C' || shape === 'Z') {
+    if (shape === 'C' || shape === 'Z') { //scissors
         return 3;
     }
     return 0;
 }
 
-function calculateScore(match) {
-    const opponent = match.charAt(0);
-    const strategy = match.charAt(2);
+function calculateScore(round) {
+    const opponent = round.charAt(0);
+    const strategy = round.charAt(2);
     const shapeScore = getShapeScore(strategy);
     if (getShapeScore(opponent) === shapeScore) {
         totalScore = totalScore + 3 + shapeScore;
@@ -41,7 +42,31 @@ function calculateScore(match) {
     return totalScore;
 }
 
+function calculateScorePartTwo(round) {
+    const opponent = round.charAt(0);
+    const strategy = round.charAt(2);
+    if (strategy === 'Y') { //draw
+        totalScorePartTwo = totalScorePartTwo + 3 + getShapeScore(opponent);
+    }
+    if (strategy === 'X') { //lose
+        if (opponent === 'A') {
+            totalScorePartTwo = totalScorePartTwo + getShapeScore('C');
+        } else if (opponent === 'B') {
+            totalScorePartTwo = totalScorePartTwo + getShapeScore('A');
+        } else totalScorePartTwo = totalScorePartTwo + getShapeScore('B');
+    }
+    if (strategy === 'Z') { //win
+        if (opponent === 'A') {
+            totalScorePartTwo = totalScorePartTwo + 6 + getShapeScore('B');
+        } else if (opponent === 'B') {
+            totalScorePartTwo = totalScorePartTwo + 6 + getShapeScore('C');
+        } else totalScorePartTwo = totalScorePartTwo + 6 + getShapeScore('A');
+    }
+    return totalScorePartTwo;
+}
+
 readline('day2/day2_input.txt').on('line', (line) => {
-    console.log(line);
-    console.log(`The total score is ${calculateScore(line)}`);
+    console.log(`Strategy for this round is: ${line}`);
+    console.log(`The total score for part one is ${calculateScore(line)}`);
+    console.log(`The total score for part two is ${calculateScorePartTwo(line)}`);
 });
